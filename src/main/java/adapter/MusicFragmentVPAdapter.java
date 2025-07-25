c4c5d28887b681e32e9ea44a0f92a740439a4640
gl <guiliu778@163.com>
2025-07-25 17:09:46 +0800

    重新布局

---
+++ src/main/java/adapter/MusicFragmentVPAdapter.java
@@ -0,0 +1,28 @@
+package adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class MusicFragmentVPAdapter extends FragmentStatePagerAdapter {
+    private List<Fragment> fragments;
+
+    public MusicFragmentVPAdapter(@NonNull FragmentManager fm,List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragments==null?null:fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
